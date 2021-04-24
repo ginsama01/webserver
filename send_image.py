ba1d933408f24e82dbe09d2d75c669ab35ae9aa3
Nguyen Minh Quang <gdra136@gmail.com>
2021-04-24 14:56:24 +0700

    send image email

--- send_image.py
+++ send_image.py
@@ -2,6 +2,10 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import logging
+import socket
+import ssl
+import base64
 
 subject = "An email with attachment from Python"
 body = "This is an email with attachment sent from Python"
@@ -9,23 +13,31 @@ sender_email = "computer.net.3000@gmail.com"
 receiver_email = "gdra136@gmail.com"
 password = 'quanghuy1234'
 
-import socket
-import ssl
-import base64
 
 
-def send_email(sender_email, password, receiver_email, msg, mailserver='smtp.gmail.com'):
+
+def send_email(sender_email, password, receiver_email, msg, mailserver='smtp.gmail.com', port=587):
+    """
+    send an byte-formatted message to a mail server
+    :param sender_email: address of sender
+    :param password: password for signing in sender_email
+    :param receiver_email: address of receiver
+    :param msg: byte-formatted message
+    :param mailserver: address of mail server
+    :param port: port number of mail server
+    :return: a status :0 ~ successfully send
+    """
     endmsg = "\r\n.\r\n"
     # Choose a mail server (e.g. Google mail server) and call it mailserver
 
     # Create socket called clientSocket and establish a TCP connection with mailserver
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    clientSocket.connect((mailserver, 587))
+    clientSocket.connect((mailserver, port))
     recv = clientSocket.recv(1024)
     print(recv)
     if recv[:3] != '220':
-        print('220 reply not received from server.')
+        logging.error('220 reply not received from server.')
 
     # Send HELO command and print server response.
     command = 'HELO Alice\r\n'
@@ -35,7 +47,7 @@ def send_email(sender_email, password, receiver_email, msg, mailserver='smtp.gma
     print(recv1)
 
     if recv1[:3] != '250':
-        print('250 reply not received from server.')
+        logging.error('250 reply not received from server.')
 
     # Request an encrypted connection
 
@@ -45,7 +57,7 @@ def send_email(sender_email, password, receiver_email, msg, mailserver='smtp.gma
     print(recv)
 
     if recv[:3] != '220':
-        print('220 reply not received from server')
+        logging.error('220 reply not received from server')
 
     # Encrypt the socket
     clientSocket = ssl.wrap_socket(clientSocket)
@@ -59,26 +71,26 @@ def send_email(sender_email, password, receiver_email, msg, mailserver='smtp.gma
     recv1 = clientSocket.recv(1024).decode()
     print(recv1)
     if recv1[:3] != '334':
-        print('334 reply not received from server')
+        logging.error('334 reply not received from server')
 
     clientSocket.send(email)
     recv1 = clientSocket.recv(1024).decode()
     print(recv1)
 
     if recv1[:3] != '334':
-        print('334 reply not received from server')
+        logging.error('334 reply not received from server')
 
     clientSocket.send(password)
     recv1 = clientSocket.recv(1024).decode()
     print(recv1)
     if recv1[:3] != '235':
-        print('235 reply not received from server')
+        logging.error('235 reply not received from server')
 
     # Send MAIL FROM command and print server response.
     clientSocket.send("MAIL FROM: <{}>\r\n".format(sender_email).encode())
     recv2 = clientSocket.recv(1024).decode()
     if recv2[:3] != '250':
-        print('250 reply not received from server.')
+        logging.error('250 reply not received from server.')
 
     # Send RCPT TO command and print server response.
     clientSocket.send("RCPT TO: <{}>\r\n".format(receiver_email).encode())
@@ -91,7 +103,7 @@ def send_email(sender_email, password, receiver_email, msg, mailserver='smtp.gma
     print(recv2)
 
     # Send data
-    clientSocket.send(msg.encode())
+    clientSocket.send(msg)
 
     # Message ends with a single period.
     clientSocket.send(endmsg.encode())
@@ -106,51 +118,46 @@ def send_email(sender_email, password, receiver_email, msg, mailserver='smtp.gma
     # Close connection with client socket
     clientSocket.close()
 
-    print('Mail sent and connection closed')
+    return 0
 
 
-def create_message(subject, body, sender_email, receiver_email, password, filename="img.jpeg", return_text=True):
-    # Create a multipart message and set headers
+def create_message(subject, body, sender_email, receiver_email, filenames=["img.jpeg"], return_text=False):
+    """
+    create a email message including text and file content.
+    :param subject: subject of email
+    :param body: textual data of email
+    :param sender_email: address of sender
+    :param receiver_email: address of receiver
+    :param filenames: name of attached files
+    :param return_text: whether return value is in textual (ascii) format or bytes format
+    :return: str or bytes
+    """
     message = MIMEMultipart()
     message["From"] = sender_email
     message["To"] = receiver_email
     message["Subject"] = subject
-    message["Bcc"] = receiver_email  # Recommended for mass emails
+    message["Bcc"] = receiver_email
 
-    # Add body to email
     message.attach(MIMEText(body, "plain"))
-
-    # In same directory as script
-
-    # Open PDF file in binary mode
-    with open(filename, "rb") as attachment:
-        # Add file as application/octet-stream
-        # Email client can usually download this automatically as attachment
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-
-    # Encode file in ASCII characters to send by email
-    encoders.encode_base64(part)
-
-    # Add header as key/value pair to attachment part
-    part.add_header(
-        "Content-Disposition",
-        f"attachment; filename= {filename}",
-    )
-
-    # Add attachment to message and convert message to string
-    message.attach(part)
+    for filename in filenames:
+        with open(filename, "rb") as attachment:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+            # Encode file in ASCII characters to send by email
+            encoders.encode_base64(part)
+            # Add header as key/value pair to attachment part
+            part.add_header(
+                "Content-Disposition",
+                f"attachment; filename= {filename}",
+            )
+            # Add attachment to message and convert message to string
+            message.attach(part)
     if return_text:
         return message.as_string()
-    return message
+    return message.as_bytes()
 
 
-# Log in to server using secure context and send email
-text = create_message(subject, 'this is another sample', sender_email, receiver_email, password)
-# print(text)
-# context = ssl.create_default_context()
-# with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-#     server.login(sender_email, password)
-#     server.sendmail(sender_email, receiver_email, text)
+text = create_message(subject='wibu', body='this is your love', sender_email=sender_email,
+                      receiver_email=receiver_email, return_text=False, filenames=['img.jpeg'])
 
 send_email(sender_email, password, receiver_email, text)
